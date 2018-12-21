d0f3856c4aa79b737e616898ebefc8dd68f596cb
meyt <pasd3000@gmail.com>
2018-12-22 01:45:42 +0330

    v0.1.1

--- microhttp_auth/__init__.py
+++ microhttp_auth/__init__.py
@@ -2,4 +2,4 @@ from .principal import JWTPrincipal, DefaultJWTPrincipal
 from .authenticator import Authenticator, authorize
 from .stateful_authenticator import StatefulAuthenticator
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
